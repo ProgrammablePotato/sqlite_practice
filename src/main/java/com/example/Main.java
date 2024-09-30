58c420d1ad82ab56c0ceb7f1415b07e36e483661
ProgrammablePotato <tamas.mihelik@gmail.com>
2024-09-30 11:11:33 +0200

    uwu2

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,12 +17,25 @@ public class Main {
         String pass = "titok";
         Connection conn = DriverManager.getConnection(url, user, pass);
         System.out.println("oksikaaaa");
+
+        String kismalac = """
+                insert into jarmuvek
+                (rendszam,marka)
+                values
+                (?,?);
+                """;
+        PreparedStatement ps = conn.prepareStatement(kismalac);
+        ps.setString(1, "ribi-01");
+        ps.setString(2, "Saang-Yong");
+        ps.executeUpdate();
+
         String sql = "select * from jarmuvek";
         Statement kiskutya = conn.createStatement();
         ResultSet rs = kiskutya.executeQuery(sql);
         while(rs.next()) {
             System.out.println(rs.getString("rendszam"));
         }
+        conn.close();
     }
 }
 
